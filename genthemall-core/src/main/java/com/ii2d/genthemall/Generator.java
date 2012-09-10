368ad5fdd434dc7ea4d343124d676629bfc89423
donilan <donilan.qq@gmail.com>
2012-09-11 01:05:56 +0800

    add some code.

--- genthemall-core/src/main/java/com/ii2d/genthemall/Generator.java
+++ genthemall-core/src/main/java/com/ii2d/genthemall/Generator.java
@@ -1,5 +1,18 @@
 package com.ii2d.genthemall;
 
-public class Generator {
+/**
+ * Generator
+ * @author Doni
+ * @since 2012-9-11
+ * @version $id$
+ */
+public interface Generator {
+	
+	/**
+	 * Generate method
+	 * @author Doni
+	 * @since 2012-9-11
+	 */
+	void generate();
 	
 }
--- genthemall-core/src/test/java/com/ii2d/genthemall/config/ConfigReaderTests.java
+++ genthemall-core/src/test/java/com/ii2d/genthemall/config/ConfigReaderTests.java
@@ -5,7 +5,8 @@ import junit.framework.TestCase;
 public class ConfigReaderTests extends TestCase {
 
 	public void testRead() {
-		ConfigReader r = new ConfigReader("target/genthemall.conf.groovy");
+		ConfigReader r = new ConfigReader("target/genthemall/genthemall.conf.groovy");
+		System.out.println(r.read());
 		assertNotNull(r.read());
 	}
 }
