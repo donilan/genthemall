0c002ea2e530c2dad95793ef29c2458783a031b3
donilan <donilan.qq@gmail.com>
2012-09-10 22:16:18 +0800

    modify template

--- genthemall-core/src/main/java/com/ii2d/genthemall/template/Template.java
+++ genthemall-core/src/main/java/com/ii2d/genthemall/template/Template.java
@@ -1,5 +1,11 @@
 package com.ii2d.genthemall.template;
 
+/**
+ * Template model
+ * @author Doni
+ * @since 2012-9-10
+ * @version $id$
+ */
 public class Template {
 	
 	protected String absulotePath;
